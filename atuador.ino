0141c62dea6fd1e3f4d2712dc12db850f3c1cc95
André Singh <92798064+quatinhe@users.noreply.github.com>
2024-04-08 09:38:00 +0100

    first actuator in project

---
+++ atuador.ino
@@ -0,0 +1,25 @@
+
+#include <Servo.h>
+
+static const int servoPin = 13;
+
+Servo servo1;
+
+void setup() {
+  Serial.begin(9600);
+  servo1.attach(servoPin);
+}
+
+void loop() {
+  for(int posDegrees = 0; posDegrees <= 180; posDegrees++) {
+    servo1.write(posDegrees);
+    Serial.println(posDegrees);
+    delay(20);
+  }
+
+  for(int posDegrees = 180; posDegrees >= 0; posDegrees--) {
+    servo1.write(posDegrees);
+    Serial.println(posDegrees);
+    delay(20);
+  }
+}
