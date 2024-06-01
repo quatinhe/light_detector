45353797b83332480deb63a1ccb807d7b233f5d9
André Singh <92798064+quatinhe@users.noreply.github.com>
2024-06-01 12:02:06 +0100

    Update atuador.ino

--- atuador.ino
+++ atuador.ino
@@ -1,5 +1,4 @@
-
-#include <Servo.h>
+#include <ESP32Servo.h>
 
 static const int servoPin = 13;
 
@@ -7,17 +6,19 @@ Servo servo1;
 
 void setup() {
   Serial.begin(9600);
-  servo1.attach(servoPin);
+  servo1.setPeriodHertz(50);    // Standard 50hz servo
+  servo1.attach(servoPin, 500, 2400); // attaches the servo on pin 13 to the servo object
+                                      // 500-2400 us are typical control pulse widths for standard servos
 }
 
 void loop() {
-  for(int posDegrees = 0; posDegrees <= 180; posDegrees++) {
-    servo1.write(posDegrees);
+  for (int posDegrees = 0; posDegrees <= 180; posDegrees++) {
+    servo1.write(posDegrees); // move the servo to position in variable 'posDegrees'
     Serial.println(posDegrees);
     delay(20);
   }
 
-  for(int posDegrees = 180; posDegrees >= 0; posDegrees--) {
+  for (int posDegrees = 180; posDegrees >= 0; posDegrees--) {
     servo1.write(posDegrees);
     Serial.println(posDegrees);
     delay(20);
